45352a7256f3dee50df35526e673fd74d47beaa9
kht711 <42952322+kht711@users.noreply.github.com>
2020-05-25 11:34:10 +0900

    Add files via upload

--- labyrinth/main.js
+++ labyrinth/main.js
@@ -1,5 +1,6 @@
 var text = [];
 var key = [];
+var bStyle = [];
 
 function readTextFile(file){
     var rawFile = new XMLHttpRequest();
@@ -20,8 +21,9 @@ function EncryptText(allText){
     let row = allText.split("\n");
     for (i = 0; i < row.length; i++){
         let r = row[i].split(", ");
-        key.push(r[0]);
-        text.push(r[1]);
+        bStyle.push(r[0])
+        key.push(r[1]);
+        text.push(r[2]);
     }
 }
 
@@ -34,6 +36,7 @@ function div(value){
     let stage = Number(document.getElementById("stage").value);
     let body = document.getElementById("body");
     if (btoa(value) == key[stage]){
+        body.className = bStyle[stage];
         body.innerHTML = decodeURI(text[stage]);
     }
 }
